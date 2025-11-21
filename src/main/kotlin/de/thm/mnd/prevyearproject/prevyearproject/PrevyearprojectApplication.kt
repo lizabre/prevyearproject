4f643cc2f87a8f675053f1a3b41dd47947e18493
lizabre <lizabredichina@gmail.com>
2025-11-21 08:20:05 +0100

    Initial commit

---
+++ src/main/kotlin/de/thm/mnd/prevyearproject/prevyearproject/PrevyearprojectApplication.kt
@@ -0,0 +1,11 @@
+package de.thm.mnd.prevyearproject.prevyearproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrevyearprojectApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrevyearprojectApplication>(*args)
+}
---
+++ src/test/kotlin/de/thm/mnd/prevyearproject/prevyearproject/PrevyearprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.thm.mnd.prevyearproject.prevyearproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrevyearprojectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
